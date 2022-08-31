61b8b683ae3f701218aec66deb044d2d5f1ff431
lenarsharipov <lenarsharipov@gmail.com>
2022-08-31 15:41:21 +0300

    7. ListIterator [#350217]

--- src/main/java/ru/job4j/iterator/ListUtils.java
+++ src/main/java/ru/job4j/iterator/ListUtils.java
@@ -5,11 +5,11 @@ import java.util.function.Predicate;
 
 public class ListUtils {
 
-    private static <T> void checkAndAdd(List<T> list, int index, T value, Predicate<ListIterator<T>> filter) {
+    public static <T> void addBefore(List<T> list, int index, T value) {
         Objects.checkIndex(index, list.size());
         ListIterator<T> iterator = list.listIterator();
         while (iterator.hasNext()) {
-            if (filter.test(iterator)) {
+            if (iterator.nextIndex() == index) {
                 iterator.add(value);
                 break;
             }
@@ -17,14 +17,16 @@ public class ListUtils {
         }
     }
 
-    public static <T> void addBefore(List<T> list, int index, T value) {
-        Predicate<ListIterator<T>> filter = i -> i.nextIndex() == index;
-        checkAndAdd(list, index, value, filter);
-    }
-
     public static <T> void addAfter(List<T> list, int index, T value) {
-        Predicate<ListIterator<T>> filter = i -> i.nextIndex() == index + 1;
-        checkAndAdd(list, index, value, filter);
+        Objects.checkIndex(index, list.size());
+        ListIterator<T> iterator = list.listIterator();
+        while (iterator.hasNext()) {
+            if (iterator.nextIndex() == index + 1) {
+                iterator.add(value);
+                break;
+            }
+            iterator.next();
+        }
     }
 
     public static <T> void removeIf(List<T> list, Predicate<T> filter) {
@@ -36,13 +38,10 @@ public class ListUtils {
         }
     }
 
-    public static <T> void removeAll(List<T> list, List<T> elements) {
-        ListIterator<T> iterator = list.listIterator();
-        while (iterator.hasNext()) {
-            if (elements.contains(iterator.next())) {
-                iterator.remove();
-            }
-        }
+    public static void main(String[] args) {
+        List<Integer> input = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7));
+        Predicate<Integer> filter = e -> e % 2 != 0;
+        ListUtils.removeIf(input, filter);
     }
 
     public static <T> void replaceIf(List<T> list, Predicate<T> filter, T value) {
@@ -53,4 +52,25 @@ public class ListUtils {
             }
         }
     }
+
+    public static <T> void removeAll(List<T> list, List<T> elements) {
+        ListIterator<T> iterator = list.listIterator();
+        while (iterator.hasNext()) {
+            if (contains(elements, iterator.next())) {
+                iterator.remove();
+            }
+        }
+    }
+
+    private static <T> boolean contains(List<T> elements, T el) {
+        boolean rsl = false;
+        for (T element : elements) {
+            if (element == el) {
+                rsl = true;
+                break;
+            }
+        }
+        return rsl;
+    }
+
 }
