b461a0f096c462ec4b10e9117b0b5d5d92d61e0c
lenarsharipov <lenarsharipov@gmail.com>
2022-09-28 14:21:47 +0300

    Утверждения с исключениями [#504886 #332412]

---
+++ src/main/java/ru/job4j/io/Config.java
@@ -0,0 +1,63 @@
+package ru.job4j.io;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringJoiner;
+
+public class Config {
+
+    private final String path;
+    private final Map<String, String> values = new HashMap<>();
+
+    public Config(final String path) {
+        this.path = path;
+    }
+
+    public void load() {
+        try (BufferedReader read = new BufferedReader(new FileReader(path))) {
+            for (String line = read.readLine(); line != null; line = read.readLine()) {
+                if (line.length() == 0 || line.startsWith("#")) {
+                    continue;
+                }
+                if (line.startsWith("=") || !line.contains("=") || (line.length() == 1 && line.charAt(0) == '=')) {
+                    throw new IllegalArgumentException();
+                }
+                int index = line.indexOf('=');
+                values.put(line.substring(0, index), line.substring(index + 1));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String value(String key) {
+        return values.get(key);
+    }
+
+    @Override
+    public String toString() {
+        StringJoiner out = new StringJoiner(System.lineSeparator());
+        try (BufferedReader read = new BufferedReader(new FileReader(this.path))) {
+            read.lines().forEach(out::add);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return out.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Config("app.properties"));
+        System.out.println(new Config("data/pair_without_comment.properties"));
+
+        String path = "./data/pair_without_comment.properties";
+        Config config = new Config(path);
+        config.load();
+        System.out.println(config.value("name2"));
+        System.out.println(config.value("name"));
+
+    }
+
+}
---
+++ src/test/java/ru/job4j/io/ConfigTest.java
@@ -0,0 +1,60 @@
+package ru.job4j.io;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+
+class ConfigTest {
+
+    @Test
+    void whenPairWithoutComment() {
+        String path = "./data/pair_without_comment.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("name")).isEqualTo("Petr Arsentev");
+    }
+
+    @Test
+    void whenPairWithoutCommentSecondName() {
+        String path = "./data/pair_without_comment.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("name2")).isEqualTo("Ivan Ivanov");
+    }
+
+    @Test
+    void whenPairWithComment() {
+        String path = "./data/pair_with_comment.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("name")).isEqualTo("Petr Arsentev");
+    }
+
+    @Test
+    void whenPairWithCommentAppFile() {
+        String path = "./app.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("hibernate.dialect")).isEqualTo("org.hibernate.dialect.PostgreSQLDialect");
+        assertThat(config.value("hibernate.connection.url")).isEqualTo("jdbc:postgresql://127.0.0.1:5432/trackstudio");
+        assertThat(config.value("hibernate.connection.driver_class")).isEqualTo("org.postgresql.Driver");
+        assertThat(config.value("hibernate.connection.username")).isEqualTo("postgres");
+        assertThat(config.value("hibernate.connection.password")).isEqualTo("password");
+    }
+
+    @Test
+    void whenPairWithCommentAndGapAndAllIllegals() {
+        String path = "./data/pair_with_comment_ends_with_equals.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("name")).isEqualTo("Ivan=");
+        assertThat(config.value("name2")).isEqualTo("name=Ivan");
+        assertThat(config.value("name3")).isEqualTo("name=");
+    }
+
+    @Test
+    void whenExceptionsDueToIncorrectTemplate() {
+        String path = "./data/pairs_without_key_value_errors.properties";
+        Config config = new Config(path);
+        assertThatThrownBy(config::load).isInstanceOf(IllegalArgumentException.class);
+    }
+}
