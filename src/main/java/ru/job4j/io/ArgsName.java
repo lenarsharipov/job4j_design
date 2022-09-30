57674b0e7f2f06980d4b44ff0c6fff3a8f75b31b
lenarsharipov <lenarsharipov@gmail.com>
2022-09-30 22:34:47 +0300

    5.2. Архивировать проект [#861 #347644]

--- src/main/java/ru/job4j/io/ArgsName.java
+++ src/main/java/ru/job4j/io/ArgsName.java
@@ -1,5 +1,6 @@
 package ru.job4j.io;
 
+import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -15,20 +16,49 @@ public class ArgsName {
     }
 
     private void parse(String[] args) {
-
         for (String argument : args) {
             int index = argument.indexOf("=");
             if (index < 2 || !argument.startsWith("-") || argument.indexOf("=") == argument.length() - 1) {
                 throw new IllegalArgumentException(String.format("Passed argument illegal - %s.", argument));
             }
+            values.put(argument.substring(1, index), argument.substring(index + 1));
+        }
+    }
 
-            if (argument.charAt(index + 1) == '*') {
-                values.put(argument.substring(1, index), argument.substring(index + 2));
-            } else {
-                values.put(argument.substring(1, index), argument.substring(index + 1));
+    private void parseForZip(String[] args) {
+        for (String argument : args) {
+            int index = argument.indexOf("=");
+            if (index < 2 || !argument.startsWith("-") || argument.indexOf("=") == argument.length() - 1) {
+                throw new IllegalArgumentException(String.format("Passed argument illegal - %s.", argument));
+            }
+            if (argument.startsWith("-d")) {
+                File directory = new File(argument.substring(index + 1));
+              if (!directory.exists()) {
+                  throw new IllegalArgumentException(String.format("Passed directory - %s does not exist", directory));
+              }
+              values.put("d", argument.substring(index + 1));
+            }
+
+            if (argument.startsWith("-o")) {
+                values.put("o", argument.substring(index + 1));
+            }
+
+            if (argument.startsWith("-e")) {
+                if (!argument.startsWith("-e=*.")) {
+                    throw new IllegalArgumentException(String.format("Passed excluded file extension illegal - %s", argument));
+                }
+                values.put("e", argument.substring(argument.indexOf("*") + 1));
             }
         }
+    }
 
+    public static ArgsName validate(String[] args) {
+        if (args.length == 0) {
+            throw new IllegalArgumentException("Parameters are null.");
+        }
+        ArgsName names = new ArgsName();
+        names.parseForZip(args);
+        return names;
     }
 
     public static ArgsName of(String[] args) {
--- src/main/java/ru/job4j/io/Zip.java
+++ src/main/java/ru/job4j/io/Zip.java
@@ -41,9 +41,9 @@ public class Zip {
                 new File("./pom.zip")
         );
 
-        Path directory = Paths.get(ArgsName.of(args).get("d"));
-        Path exclude = Paths.get(ArgsName.of(args).get("e"));
-        File output = Paths.get(ArgsName.of(args).get("o")).toFile();
+        Path directory = Paths.get(ArgsName.validate(args).get("d"));
+        Path exclude = Paths.get(ArgsName.validate(args).get("e"));
+        File output = Paths.get(ArgsName.validate(args).get("o")).toFile();
         Predicate<Path> condition = p -> !p.toFile().getName().endsWith(exclude.toString());
         List<Path> sources = Search.search(directory, condition);
         zip.packFiles(sources, output);
