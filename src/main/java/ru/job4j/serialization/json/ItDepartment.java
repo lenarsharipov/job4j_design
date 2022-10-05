e6ffdf72dc6c7b41e9febc461a00c730f4d98e1c
lenarsharipov <lenarsharipov@gmail.com>
2022-10-05 22:42:43 +0300

    2. Формат JSON [#313164]

---
+++ src/main/java/ru/job4j/serialization/json/ItDepartment.java
@@ -0,0 +1,30 @@
+package ru.job4j.serialization.json;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+public class ItDepartment {
+    public static void main(String[] args) {
+        final Employee employee = new Employee("Tom", 1, false,
+                new String[] {"Java Middle Developer", "Java Junior Developer"},
+                new Stack("Java", "Maven", "Spring"));
+
+        final Gson gson = new GsonBuilder().create();
+        System.out.println(gson.toJson(employee));
+
+        final String employeeJson =
+                "{"
+                        + "\"name\":Bob,"
+                        + "\"id\":17,"
+                        + "\"retired\":true,"
+                        + "\"positions\":"
+                        + "[\"Java Junior Developer\"],"
+                        + "\"stack\":"
+                        + "{"
+                        + "\"pL\":\"Java\",\"buildTool\":\"Ant\",\"webFramework\":\"Spark\""
+                        + "}"
+                        + "}";
+        final Employee employeeMod = gson.fromJson(employeeJson, Employee.class);
+        System.out.println(employeeMod);
+    }
+}
---
+++ src/main/java/ru/job4j/serialization/json/Stack.java
@@ -0,0 +1,22 @@
+package ru.job4j.serialization.json;
+
+public class Stack {
+    private final String pL;
+    private final String buildTool;
+    private final String webFramework;
+
+    public Stack(String pL, String buildTool, String webFramework) {
+        this.pL = pL;
+        this.buildTool = buildTool;
+        this.webFramework = webFramework;
+    }
+
+    @Override
+    public String toString() {
+        return "Stack{"
+                + "programmingLanguage='" + pL + '\''
+                + ", buildTool='" + buildTool + '\''
+                + ", webFramework='" + webFramework + '\''
+                + '}';
+    }
+}
