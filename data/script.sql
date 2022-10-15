7299a1d3d1b414b8c20ea3727d59ff2403578bb7
lenarsharipov <lenarsharipov@gmail.com>
2022-10-15 22:15:00 +0300

    0. Что такое база данных. [#6860]

--- data/script.sql
+++ data/script.sql
@@ -1,1 +1,19 @@
-delete from employee;
+create table employees (
+	id serial primary key,
+	name varchar(255),
+	surname varchar(255),
+	date date,
+	salary decimal
+);
+
+insert into employees(name, surname, date, salary) values('Ivan', 'Ivanov', '2022-10-15', '42500.500');
+
+select * from employees;
+
+update employees set name = 'Petr', surname = 'Petrov', date = '2019-01-11', salary = '60000';
+
+select * from employees;
+
+delete from employees;
+
+select * from employees;
