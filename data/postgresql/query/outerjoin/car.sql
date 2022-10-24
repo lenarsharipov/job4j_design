d277ddb9b9c7484b6aecee56bdc1c5dcf40a98d9
lenarsharipov <lenarsharipov@gmail.com>
2022-10-24 13:25:48 +0300

    Хранилище машин [#1733]

---
+++ data/postgresql/query/outerjoin/car.sql
@@ -0,0 +1,33 @@
+create table car_bodies (
+	id serial primary key,
+	name varchar(200)
+);
+
+create table car_engines (
+	id serial primary key,
+	name varchar(200)
+);
+
+create table car_transmissions (
+	id serial primary key,
+	name varchar(200)
+);
+
+create table cars (
+	id serial primary key,
+	name varchar(200),
+	body_id int references car_bodies(id),
+	engine_id int references car_engines(id),
+	transmission_id int references car_transmissions(id)
+);
+
+insert into car_bodies(name) values ('sedan'), ('minivan'), ('crossover'), ('off-road'), ('cabriolet'), ('truck');
+insert into car_engines(name) values ('petrol engine'), ('gas engine'), ('diesel engine'), ('hybrid engine'), ('electric engine');
+insert into car_transmissions(name) values ('manual'), ('automatic'), ('semi-automatic'), ('cvt'), ('robot');
+insert into cars(name, body_id, engine_id, transmission_id) values  ('bmw',     1, 1, 2),
+																	('toyota',  2, 2, 4),
+																	('lada',    3, 1, 1),
+																	('peugeot', 1, 3, 1);
+insert into cars(name, body_id) values  ('UAZ', 4);
+insert into cars(name, engine_id) values  ('TAVRIA', 1);
+insert into cars(name, transmission_id) values  ('CITROEN', 1);
---
+++ data/postgresql/query/outerjoin/car_filter.sql
@@ -0,0 +1,44 @@
+/*
+Вывести список всех машин и все привязанные к ним детали.
+Нужно учесть, что каких-то деталей машина может и не содержать.
+В таком случае значение может быть null при выводе (например,
+название двигателя null);
+*/
+select c.name Авто, cb.name Кузов, ce.name Двигатель, ct.name Трансмиссия
+from cars c
+left join car_bodies cb ON cb.id = c.body_id
+left join car_engines ce ON ce.id = c.engine_id
+left join car_transmissions ct ON ct.id = c.transmission_id
+
+
+/*
+Вывести кузовы, которые не используются НИ в одной машине.
+*/
+select c.name Авто, cb.name Кузов, ce.name Двигатель, ct.name Трансмиссия
+from car_bodies cb
+left join cars c on cb.id = c.body_id
+left join car_engines ce on ce.id = c.engine_id
+left join car_transmissions ct on ct.id = c.transmission_id
+where c.body_id is null;
+
+
+/*
+Вывести двигатели, которые не используются НИ в одной машине
+*/
+select c.name Авто, cb.name Кузов, ce.name Двигатель, ct.name Трансмиссия
+from car_engines ce
+left join cars c on ce.id = c.engine_id
+left join car_bodies cb on cb.id = c.body_id
+left join car_transmissions ct on ct.id = c.transmission_id
+where c.engine_id is null;
+
+
+/*
+Вывести коробки передач, которые не используются НИ в одной машине
+*/
+select c.name Авто, cb.name Кузов, ce.name Двигатель, ct.name Трансмиссия
+from car_transmissions ct
+left join cars c on ct.id = c.transmission_id
+left join car_engines ce on ce.id = c.engine_id
+left join car_bodies cb on cb.id = c.body_id
+where c.transmission_id is null;
