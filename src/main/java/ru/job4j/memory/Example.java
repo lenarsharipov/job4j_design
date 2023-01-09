3eb316b3fe35cc871e175b9ffd52c5ed63a179e8
lenarsharipov <lenarsharipov@gmail.com>
2023-01-09 21:10:27 +0300

    1. Stack, Heap, Metaspace [#504932 #379309]

---
+++ src/main/java/ru/job4j/memory/Example.java
@@ -0,0 +1,14 @@
+package ru.job4j.memory;
+
+public class Example {
+    public void action(Object parameter) {
+        String str = parameter.toString();
+        System.out.println(str);
+    }
+    public static void main(String[] args) {
+        int x = 0;
+        Object obj = new Object();
+        Example example = new Example();
+        example.action(obj);
+    }
+}
