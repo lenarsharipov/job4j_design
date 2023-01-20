c2df8e10d08bea708356ccace574a20b623371c2
lenarsharipov <lenarsharipov@gmail.com>
2023-01-20 10:47:47 +0300

    1. Реализация кеша на SoftReference [#1592 #382518]

--- src/main/java/ru/job4j/cache/AbstractCache.java
+++ src/main/java/ru/job4j/cache/AbstractCache.java
@@ -21,4 +21,5 @@ public abstract class AbstractCache<K, V> {
     }
 
     protected abstract V load(K key);
+
 }
--- src/main/java/ru/job4j/cache/DirFileCache.java
+++ src/main/java/ru/job4j/cache/DirFileCache.java
@@ -28,4 +28,5 @@ public class DirFileCache extends AbstractCache<String, String> {
         }
         return out;
     }
+
 }
