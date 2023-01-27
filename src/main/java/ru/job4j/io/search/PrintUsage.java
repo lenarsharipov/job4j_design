33f29bb35f28b90fed5e781aef879c857bb4089a
lenarsharipov <lenarsharipov@gmail.com>
2023-01-27 15:14:41 +0300

    3. Классы вывода PrintStream / PrintWriter [#504999 #385546]

--- src/main/java/ru/job4j/io/search/PrintUsage.java
+++ src/main/java/ru/job4j/io/search/PrintUsage.java
@@ -3,6 +3,7 @@ package ru.job4j.io.search;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintStream;
+import java.io.PrintWriter;
 
 public class PrintUsage {
     public static void main(String[] args) {
@@ -14,6 +15,12 @@ public class PrintUsage {
 
         try (FileOutputStream out = new FileOutputStream("data/print2.txt")) {
             out.write("Запись при помощи FileOutputStream".getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try (PrintWriter writer = new PrintWriter("data/print3.txt")) {
+            writer.println("Запись строки при помощи символьного способа классом PrintWriter");
         } catch (IOException e) {
             e.printStackTrace();
         }
