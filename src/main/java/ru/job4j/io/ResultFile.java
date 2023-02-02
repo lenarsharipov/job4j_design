f7e646ed0d35be721f71543258b75b83adfc651f
lenarsharipov <lenarsharipov@gmail.com>
2023-02-02 12:37:08 +0300

    5.2. BufferedOutputStream [#252490 #346672]

--- src/main/java/ru/job4j/io/ResultFile.java
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -1,19 +1,49 @@
 package ru.job4j.io;
 
-import java.io.BufferedOutputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
 
 public class ResultFile {
     public static void main(String[] args) {
-        try (PrintWriter out = new PrintWriter(
-                new BufferedOutputStream(
-                        new FileOutputStream("./data/result.txt")
-                ))) {
-            out.println("Hello, world!");
+        writeBytes();
+        writeSymbols();
+        bufferedWriteBytes();
+        bufferedWriteSymbols();
+    }
+
+    private static void writeBytes() {
+        try (var out = new FileOutputStream("data/resultWriteBytes.txt")) {
+                out.write(("Hello world!" + System.lineSeparator()).getBytes());
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
+    private static void writeSymbols() {
+        try (var out = new FileWriter("data/resultWriteSymbols.txt")) {
+                out.write("Hello World" + System.lineSeparator());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void bufferedWriteBytes() {
+        try (var out = new BufferedOutputStream(
+                new FileOutputStream("data/resultBufferedBytesWriter.txt"))) {
+            out.write(("Hello world" + System.lineSeparator()).getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void bufferedWriteSymbols() {
+        try (var out = new PrintWriter(
+                new BufferedOutputStream(
+                        new FileOutputStream("data/resultPrintWriter.txt")))) {
+            out.println("Hello world");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }
